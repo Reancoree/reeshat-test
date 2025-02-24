ffaf6d5be3b83817e583bed198580ea5460ef953
Reancoree <reancoree@gmail.com>
2025-02-24 21:49:12 +0300

    вернул доступ к secret_key

--- project/project/settings.py
+++ project/project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+SECRET_KEY = 'django-insecure-d1=n@9(*n$mb#s)wv276-ps1=+kjk6bep607a)y#m4(+8d%6f-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
